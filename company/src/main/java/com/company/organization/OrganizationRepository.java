d70ad428dfe29a428f4f95635ed0d70ab82b48af
jayaree999 <jayyqwe123@gmail.com>
2022-08-24 13:25:01 +0700

    delete unused

--- company/src/main/java/com/company/organization/OrganizationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.organization;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OrganizationRepository extends MongoRepository<Organization, String> {
-
-}
